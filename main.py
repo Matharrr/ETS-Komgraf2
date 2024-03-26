67cff4931c7534922886128891fe2a416e4e9c58
Matharrr <117796799+Matharrr@users.noreply.github.com>
2024-03-26 14:55:08 +0700

    fix simulasi2D

--- main.py
+++ main.py
@@ -13,14 +13,31 @@ awan1 = Awan(150, 100)
 awan2 = Awan(260, 80)
 awan3 = Awan(700, 120)
 awan4 = Awan(810, 150)
+
 teratai1 = Teratai(800, 300)
 teratai2 = Teratai(600, 300)
 teratai3 = Teratai(400, 300)
+
 rumput = Ilalang(172, 255)
+
 egg = Telur(185, 290)
+egg1 = Telur(195, 290)
+egg2 = Telur(205, 290)
+egg3 = Telur(215, 290)
+egg4 = Telur(225, 290)
+egg5 = Telur(195, 298)
+egg6 = Telur(205, 298)
+egg7 = Telur(215, 298)
+egg8 = Telur(197, 306)
+egg9 = Telur(185, 306)
+egg10 = Telur(195, 306)
+
 cebong = Kecebong(307, 438)
+
 cebong_kaki  = Kecebong_Berkaki(500, 500)
+
 Katak_muda = KatakMuda(701, 427)
+
 Katak_dewasa = KatakDewasa(800, 280)
 
 def setup():
@@ -29,18 +46,37 @@ def setup():
 
 def draw():
     primitif.basic.draw_wavy_line(0, 300, 1000, 300, 8, 12, c=[100,149,207, 255])
+    
     awan1.draw()
     awan2.draw()
     awan3.draw()
     awan4.draw()
+    
     rumput.draw()
+    
     teratai1.draw()
     teratai2.draw()
     teratai3.draw()
+    
     egg.draw()
+    
+    egg1.draw()
+    egg2.draw()
+    egg3.draw()
+    egg4.draw()
+    egg5.draw()
+    egg6.draw()
+    egg7.draw()
+    egg8.draw()
+    egg9.draw()
+    egg10.draw()
+    
     cebong.draw()
+    
     cebong_kaki.draw()
+    
     Katak_muda.draw()
+    
     Katak_dewasa.draw()
     
 def settings():
